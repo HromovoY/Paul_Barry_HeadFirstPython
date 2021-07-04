09b8d9b75eaeb90efd3a87e4990086db8f7f4d1d
konstantin <araskrudjmakdak@gmail.com>
2021-07-04 18:01:03 +0500

    Add repit with waiting 1-60 sec

--- chapter_1/odd.py
+++ chapter_1/odd.py
@@ -1,13 +1,18 @@
 from datetime import datetime
+import random
+import time
 
 
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29,
         31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
 
-right_now_minute = datetime.today().minute
+for i in range(5):
+    right_now_minute = datetime.today().minute
 
-if right_now_minute in odds:
-    print('This minute seems a little odd.')
-else:
-    print('Not an odd minute')
+    if right_now_minute in odds:
+        print('This minute seems a little odd.')
+    else:
+        print('Not an odd minute')
+    time_wait = random.randint(1, 60)
+    time.sleep(time_wait)
